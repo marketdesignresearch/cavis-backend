98c4e8f6ba3b9fdcf6861503e4e46cf2b387e019
fabio <islerfab@gmail.com>
2019-07-09 15:20:36 +0400

    feat(api): added swagger & swagger-ui

--- src/main/kotlin/org/marketdesignresearch/cavisbackend/CavisBackendApplication.kt
+++ src/main/kotlin/org/marketdesignresearch/cavisbackend/CavisBackendApplication.kt
@@ -12,12 +12,25 @@ import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.math.BigDecimal
 
 @SpringBootApplication
+@EnableSwagger2
 class CavisBackendApplication {
 
     private val logger = LoggerFactory.getLogger(CavisBackendApplication::class.java)
+
+    @Bean
+    fun api(): Docket = Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.ant("/auctions/**"))
+            .build()
 
     @Bean
     fun init() = ApplicationRunner {
